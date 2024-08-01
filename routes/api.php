1c67173fe37bf787e6b8b07f61a8da0497187084
megalobob <phanunsuon@gmail.com>
2024-08-01 15:48:14 +0700

    1.01

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [\App\Http\Controllers\Api\UserController::class, 'createUser']);
 Route::post('/login', [\App\Http\Controllers\Api\UserController::class, 'loginUser']);
 
+Route::prefix('v1')->group(function () {
+  Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('simple', [\App\Http\Controllers\Api\UserController::class, 'simple']);
+  });
+});
 // Route::group(['middleware' => ['auth:sanctum'], function () {
 //     Route::post('/hello', [\App\Http\Controllers\Api\UserController::class, 'hello']);
 // }]);
